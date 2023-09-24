af7ede5430a579f3d11d6f1b30daf1d5139889e8
AbdulwahabJ <67449655+AbdulwahabJ@users.noreply.github.com>
2023-09-24 23:46:51 +0300

    add custom list view item to home view body

---
+++ lib/featurse/home/presentaion/views/widgets/custom_list_view_item.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../core/utils/assets.dart';
+
+class CustomeListViewItem extends StatelessWidget {
+  const CustomeListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * .3,
+      child: AspectRatio(
+        aspectRatio: 2.7 / 4,
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            color: Colors.amber,
+            image: const DecorationImage(
+              image: AssetImage(AssetsData.testImage),
+              fit: BoxFit.fill,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
