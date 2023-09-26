556d6aeea9904bf72b80a39c32002ab95dbac6ed
AbdulwahabJ <67449655+AbdulwahabJ@users.noreply.github.com>
2023-09-26 22:08:47 +0300

    best seller list view done

--- lib/featurse/home/presentaion/views/widgets/home_view_body.dart
+++ lib/featurse/home/presentaion/views/widgets/home_view_body.dart
@@ -1,7 +1,7 @@
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
-import 'best_seller_list_view_item.dart';
+import 'best_seller_list_view.dart';
 import 'custom_app_bar.dart';
 import 'featured_list_view.dart';
 
@@ -10,25 +10,36 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
-      child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            CustomAppBar(),
-            SizedBox(height: 14),
-            FeaturedListView(),
-            SizedBox(height: 40),
-            Text(
-              'Best Seller',
-              style: Styles.textStyle18,
+    return const CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(
+          child: SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CustomAppBar(),
+                SizedBox(height: 8),
+                FeaturedListView(),
+                SizedBox(height: 35),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Text(
+                    'Best Seller',
+                    style: Styles.textStyle18,
+                  ),
+                ),
+                SizedBox(height: 12),
+              ],
             ),
-            SizedBox(height: 20),
-            BestSellerListViewItem(),
-          ],
+          ),
         ),
-      ),
+        SliverFillRemaining(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.0),
+            child: BestSellerListView(),
+          ),
+        ),
+      ],
     );
   }
 }
