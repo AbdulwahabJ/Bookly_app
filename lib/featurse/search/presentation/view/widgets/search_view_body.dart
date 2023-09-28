3235da6548b0007e524d5ec616c10df447455910
AbdulwahabJ <67449655+AbdulwahabJ@users.noreply.github.com>
2023-09-28 04:06:51 +0300

    add search list view result

--- lib/featurse/search/presentation/view/widgets/search_view_body.dart
+++ lib/featurse/search/presentation/view/widgets/search_view_body.dart
@@ -1,5 +1,7 @@
+import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
+import '../../../../home/presentaion/views/widgets/best_seller_list_view_item.dart';
 import 'custom_search_text_field.dart';
 
 class SearchViewBody extends StatelessWidget {
@@ -10,10 +12,38 @@ class SearchViewBody extends StatelessWidget {
     return const Padding(
       padding: EdgeInsets.symmetric(horizontal: 20.0),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           CustomSearchTextField(),
+          SizedBox(height: 30),
+          Text(
+            'Search Result',
+            style: Styles.textStyle18,
+          ),
+          SizedBox(height: 6),
+          Expanded(child: SearchResultLisView()),
         ],
       ),
+    );
+  }
+}
+
+class SearchResultLisView extends StatelessWidget {
+  const SearchResultLisView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      // shrinkWrap: true,
+      // physics: const NeverScrollableScrollPhysics(),
+      padding: EdgeInsets.zero,
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return const Padding(
+          padding: EdgeInsets.symmetric(vertical: 10.0),
+          child: BookListViewItem(),
+        );
+      },
     );
   }
 }
