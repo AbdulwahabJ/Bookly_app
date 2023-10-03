0f2c887204188122ea6e644966845ca22a33a831
AbdulwahabJ <67449655+AbdulwahabJ@users.noreply.github.com>
2023-10-03 08:35:46 +0300

    implement fetch newest books

--- lib/featurse/home/data/models/repos/home_repo_impl.dart
+++ lib/featurse/home/data/models/repos/home_repo_impl.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: unused_local_variable, empty_catches
+
 import 'package:bookly_app/core/errors/failures.dart';
+import 'package:bookly_app/core/utils/api_service.dart';
 
 import 'package:bookly_app/featurse/home/data/models/book_model/book_model.dart';
 
@@ -6,11 +9,26 @@ import 'package:dartz/dartz.dart';
 
 import 'home_repo.dart';
 
-class HomeRepoImpl extends HomeRepo{
+class HomeRepoImpl extends HomeRepo {
+  final ApiService apiService;
+
+  HomeRepoImpl(this.apiService);
+  //
   @override
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
-    // TODO: implement fetchBestSellerBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchNewestBooks() async {
+    try {
+      var data = await apiService.get(
+          endPoint:
+              'volumes?Filtering=free-ebooks&Sorting=newest&q=subject:Programming');
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+      // ignore: unused_catch_clause
+    } on Exception catch (e) {
+      return left(ServerFailure());
+    }
   }
 
   @override
